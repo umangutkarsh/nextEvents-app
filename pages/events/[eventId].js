c471a7f1c59f4496dc06c55b606c9b4f1a1a554a
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-06 13:27:19 +0530

    adding event-detail page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,33 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 function EventDetailPage() {
+	const router = useRouter();
+	const event = getEventById(router.query.eventId);
+
+	if (!event) {
+		return (
+			<div>
+				<h2>No Event Found</h2>
+			</div>
+		);
+	}
+
 	return (
-		<div>
-			<h1>Event Detail</h1>
-		</div>
+		<>
+			<EventSummary title={event.title} />
+			<EventLogistics
+				date={event.date}
+				address={event.location}
+				image={event.image}
+				imageAlt={event.title}
+			/>
+			<EventContent>{event.description}</EventContent>
+		</>
 	);
 }
 
