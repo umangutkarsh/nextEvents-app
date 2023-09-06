39e6e67832e8c9201837b7e1c445e41f646b423e
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-06 15:34:07 +0530

    extracting data from catch-all page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,36 @@
+import { useRouter } from 'next/router';
+import { getFilteredEvents } from '../../dummy-data';
+
 function FilteredEventsPage() {
+	const router = useRouter();
+
+	const filterData = router.query.slug;
+
+	if (!filterData) {
+		return <p className='center'>Loading</p>;
+	}
+
+	const year = Number(filterData[0]);
+	const month = Number(filterData[1]);
+
+	if (
+		isNaN(year) ||
+		isNaN(month) ||
+		(year < 2021 && year > 2030) ||
+		(month < 1 && month > 12)
+	) {
+		return <p>Invalid URL. Please adjust values</p>;
+	}
+
+	const filteredEvents = getFilteredEvents({
+		year,
+		month,
+	});
+
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return <p>No Events Found</p>;
+	}
+
 	return (
 		<div>
 			<h1>Filtered Page</h1>
