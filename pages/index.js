dd3fd4d11503f0868cc8491ad0f2ded2293d7f0e
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-13 16:34:22 +0530

    adding ssg on home-page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
 import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
+import { getFeaturedEvents as dummyFeaturedEvents } from '../dummy-data';
 
-function HomePage() {
-	const featuredEvents = getFeaturedEvents();
+function HomePage(props) {
+	const featuredEvents = dummyFeaturedEvents();
 
 	return (
 		<div>
-			<EventList items={featuredEvents} />
+			<EventList items={props.events} />
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	const featuredEvents = await getFeaturedEvents();
+
+	return {
+		props: {
+			events: featuredEvents,
+		},
+	};
 }
 
 export default HomePage;
