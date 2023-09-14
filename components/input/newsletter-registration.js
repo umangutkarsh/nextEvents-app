b2f2b678bad718d9f5f8bb87e5d6679aa27ac5fa
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-14 23:47:58 +0530

    setting up database ans running queries

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,8 +1,10 @@
-import { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import classes from './newsletter-registration.module.css';
 
 function NewsletterRegistration() {
-	const emailInputRef = useRef();
+	// const emailInputRef = useRef();
+	const [email, setEmail] = useState('');
+	const [signed, setSigned] = useState('');
 
 	function registrationHandler(event) {
 		event.preventDefault();
@@ -11,35 +13,47 @@ function NewsletterRegistration() {
 		// optional: validate input
 		// send valid data to API
 
-		const enteredEmail = emailInputRef.current.value;
+		// let enteredEmail = emailInputRef.current.value;
 
 		fetch('/api/newsletter', {
 			method: 'POST',
-			body: JSON.stringify({ email: enteredEmail }),
+			body: JSON.stringify({ email: email }),
 			headers: {
 				'Content-Type': 'application/json',
 			},
 		})
 			.then(response => response.json())
 			.then(data => console.log(data));
+
+		setEmail('');
+		setSigned('Signed Up!');
 	}
 
 	return (
-		<section className={classes.newsletter}>
-			<h2>Sign up to stay updated!</h2>
-			<form onSubmit={registrationHandler}>
-				<div className={classes.control}>
-					<input
-						type='email'
-						id='email'
-						placeholder='Your email'
-						aria-label='Your email'
-						ref={emailInputRef}
-					/>
-					<button onClick={registrationHandler}>Register</button>
-				</div>
-			</form>
-		</section>
+		<React.Fragment>
+			{signed && (
+				<p className='center' style={{ marginTop: '2rem' }}>
+					{signed}
+				</p>
+			)}
+			<section className={classes.newsletter}>
+				<h2>Sign up to stay updated!</h2>
+				<form onSubmit={registrationHandler}>
+					<div className={classes.control}>
+						<input
+							type='email'
+							id='email'
+							placeholder='Your email'
+							aria-label='Your email'
+							// ref={emailInputRef}
+							value={email}
+							onChange={event => setEmail(event.target.value)}
+						/>
+						<button onClick={registrationHandler}>Register</button>
+					</div>
+				</form>
+			</section>
+		</React.Fragment>
 	);
 }
 
