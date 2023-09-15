56f3eddf7cf10afa22b038ebde96817b27f95713
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-09-15 12:34:02 +0530

    working on comment-notifications

--- components/input/comments.js
+++ components/input/comments.js
@@ -1,21 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import NotificationContext from '../../store/notification-context';
 
 function Comments(props) {
 	const { eventId } = props;
 
+	const notificationCtx = useContext(NotificationContext);
+
 	const [showComments, setShowComments] = useState(false);
 	const [comments, setComments] = useState([]);
 
+	const [isFetchingComments, setIsFetchingComments] = useState(false);
+
 	useEffect(() => {
 		if (showComments) {
+			setIsFetchingComments(true);
 			fetch(`/api/comments/${eventId}`)
 				.then(response => response.json())
 				.then(data => {
 					setComments(data.comments);
+					setIsFetchingComments(false);
+				})
+				.catch(error => {
+					notificationCtx.showNotification({
+						title: 'Error!',
+						message: error.message || 'Something went wrong',
+						status: 'error',
+					});
 				});
 		}
 	}, [showComments]);
@@ -26,6 +40,13 @@ function Comments(props) {
 
 	function addCommentHandler(commentData) {
 		// send data to API
+
+		notificationCtx.showNotification({
+			title: 'Posting...',
+			message: 'Posting comment',
+			status: 'pending',
+		});
+
 		fetch(`/api/comments/${eventId}`, {
 			method: 'POST',
 			body: JSON.stringify(commentData),
@@ -33,8 +54,29 @@ function Comments(props) {
 				'Content-Type': 'application/json',
 			},
 		})
-			.then(response => response.json())
-			.then(data => console.log(data));
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				}
+
+				return response.json().then(data => {
+					throw new Error(data.message || 'Something went wrong');
+				});
+			})
+			.then(data => {
+				notificationCtx.showNotification({
+					title: 'Success!',
+					message: 'Comment posted successfully',
+					status: 'success',
+				});
+			})
+			.catch(error => {
+				notificationCtx.showNotification({
+					title: 'Error!',
+					message: error.message || 'Something went wrong',
+					status: 'error',
+				});
+			});
 	}
 
 	return (
@@ -43,7 +85,10 @@ function Comments(props) {
 				{showComments ? 'Hide' : 'Show'} Comments
 			</button>
 			{showComments && <NewComment onAddComment={addCommentHandler} />}
-			{showComments && <CommentList comments={comments} />}
+			{isFetchingComments && <p>Loading...</p>}
+			{showComments && !isFetchingComments && (
+				<CommentList comments={comments} />
+			)}
 		</section>
 	);
 }
--- components/input/new-comment.js
+++ components/input/new-comment.js
@@ -40,34 +40,16 @@ function NewComment(props) {
 			<div className={classes.row}>
 				<div className={classes.control}>
 					<label htmlFor='email'>Your email</label>
-					<input
-						type='email'
-						id='email'
-						value={email}
-						onChange={event => setEmail(event.target.value)}
-						ref={emailInputRef}
-					/>
+					<input type='email' id='email' ref={emailInputRef} />
 				</div>
 				<div className={classes.control}>
 					<label htmlFor='name'>Your name</label>
-					<input
-						type='text'
-						id='name'
-						value={name}
-						onChange={event => setName(event.target.value)}
-						ref={nameInputRef}
-					/>
+					<input type='text' id='name' ref={nameInputRef} />
 				</div>
 			</div>
 			<div className={classes.control}>
 				<label htmlFor='comment'>Your comment</label>
-				<textarea
-					id='comment'
-					rows='5'
-					value={comment}
-					onChange={event => setComment(event.target.value)}
-					ref={commentInputRef}
-				></textarea>
+				<textarea id='comment' rows='5' ref={commentInputRef}></textarea>
 			</div>
 			{isInvalid && <p>Please enter a valid email address and comment!</p>}
 			<button>Submit</button>
